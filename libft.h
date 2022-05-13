1d747d9ff35a0c2a3ac4a58a1c5c1c37d432b4eb
Aitor Lopez de audicana oar <ailopez-@car10s5.42barcelona.com>
2022-05-13 20:34:50 +0200

    strjoin, substr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ailopez- <ailopez-@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 17:43:43 by ailopez-          #+#    #+#             */
-/*   Updated: 2022/05/13 19:08:06 by ailopez-         ###   ########.fr       */
+/*   Updated: 2022/05/13 19:15:22 by ailopez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -40,4 +40,5 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t leni);
 int		ft_atoi(const char *str);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 #endif
