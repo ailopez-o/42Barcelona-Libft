105412312b713fa5dd9787bdac95a383b858800d
Aitor Lopez de Audicana <aitorlopezdeaudicana@iMac-de-Aitor.local>
2022-05-13 12:44:04 +0200

    substr

---
+++ ft_substr.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aitorlopezdeaudicana <marvin@42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/13 12:29:55 by aitorlope         #+#    #+#             */
+/*   Updated: 2022/05/13 12:43:33 by aitorlope        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+char *ft_substr(char const *s, unsigned int start,size_t len)
+{
+	char	*ptr;
+
+	ptr = malloc (len * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+
+
+
+
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ailopez- <ailopez-@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 17:43:43 by ailopez-          #+#    #+#             */
-/*   Updated: 2022/05/12 17:51:38 by ailopez-         ###   ########.fr       */
+/*   Updated: 2022/05/13 12:30:39 by aitorlope        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -39,4 +39,5 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t leni);
 int		ft_atoi(const char *str);
+char 	*ft_substr(char const *s, unsigned int start,size_t len);
 #endif
