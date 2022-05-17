625a2a4915f27b1eb3f445bdd5b4fc1a6c395b40
Aitor Lopez de audicana oar <ailopez-@cbr11s1.42barcelona.com>
2022-05-17 12:18:05 +0200

    putend, putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,16 +6,32 @@
 /*   By: aitorlopezdeaudicana <marvin@42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 16:50:37 by aitorlope         #+#    #+#             */
-/*   Updated: 2022/05/16 16:57:11 by aitorlope        ###   ########.fr       */
+/*   Updated: 2022/05/17 12:15:43 by ailopez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h" 
 
 void ft_putnbr_fd(int n, int fd)
 {
-	char	*nbr;
+	char	digit;
 
-	nbr = ft_itoa(n);
-	write(fd, nbr, ft_strlen(nbr));
-	free(nbr);
+	if (n == -2147483648)
+		write(fd, "-2147483648", 11);
+	else if (n < 0)
+	{
+		write(fd, "-", 1);
+		n = -n;
+		ft_putnbr_fd(n, fd);
+	}
+	else if (n >= 10)
+	{		
+		ft_putnbr_fd((n / 10), fd);
+		digit = (n % 10) + 48;
+		write(fd, &digit, 1);
+	}
+	else
+	{
+		n = n + 48;
+		write(fd, &n, 1);
+	}
 }
