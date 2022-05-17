d4e3bb8b2a3b4f45407a463c2895aec988625d40
Aitor Lopez de audicana oar <ailopez-@cbr11s1.42barcelona.com>
2022-05-17 12:16:28 +0200

    putnbr v1

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,12 +6,12 @@
 /*   By: aitorlopezdeaudicana <marvin@42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 16:41:16 by aitorlope         #+#    #+#             */
-/*   Updated: 2022/05/16 16:43:49 by aitorlope        ###   ########.fr       */
+/*   Updated: 2022/05/17 11:59:58 by ailopez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	ft_putstr_fd(char *s, int fd)
 {
-		write(fd,s,ft_strlen(s));
+	write(fd, s, ft_strlen(s));
 }
